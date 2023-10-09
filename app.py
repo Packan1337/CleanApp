b0d9300445e8ef67811c41834348de95dbe4bc20
CharlieRosander <charlie.plugga@gmail.com>
2023-10-09 11:48:39 +0200

    Kopplade mysql db till koden

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-
 from flask import render_template, redirect, url_for, flash, request, Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from db_models import db, User
+import MySQLdb
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'mysql+mysqlconnector://kaliber:cleanapp@kaliber.mysql.pythonanywhere-services.com/kaliber$cleanapp_db'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
 
 # Run app #
 if __name__ == '__main__':
---
+++ db_models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
