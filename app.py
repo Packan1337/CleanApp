27c44b00c292bac0a7d7b944e37500c1da380a79
CharlieRosander <charlie.plugga@gmail.com>
2023-10-10 17:31:21 +0200

    ändrade URI och tog bort lite skit

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ from db_models import db, User
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+mysqlconnector://root:kaliberai@localhost/cleanapp_db'
+    'mysql+mysqlconnector://kaliber:cleanapp@kaliber.mysql.pythonanywhere-services.com/kaliber$cleanapp_db'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+
 def insert_user():
     new_user = User(
-        first_name='testuser', 
-        last_name="testname", 
-        password="asd", 
+        first_name='testuser',
+        last_name="testname",
+        password="asd",
         email='testuser@example.com')
-    
+
     db.session.add(new_user)
     db.session.commit()
 
@@ -27,19 +28,20 @@ def insert_user():
 def index():
     return render_template('index.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
-
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 # Run app #
+# Remove this when deploying to pythonanywhere
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # insert_user()
         app.run(host='0.0.0.0', port=80, debug=True)
