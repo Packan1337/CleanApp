7488e36e718618342f05ab4c9e4b0d582a83fd7c
CharlieRosander <charlie.plugga@gmail.com>
2023-10-16 09:52:05 +0200

    flyttade 2 rader kod för att auto-skapa tables

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 @app.route('/index')
@@ -121,6 +122,5 @@ def logout():
 # Run app #
 # Remove this when deploying to pythonanywhere
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
+        
