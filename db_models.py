849e48fc0557a3d3bb9af388d007ec1522eb8e6a
CharlieRosander <charlie.plugga@gmail.com>
2023-10-24 21:03:44 +0200

    changes in changelog.md

--- db_models.py
+++ db_models.py
@@ -5,52 +5,35 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), unique=False, nullable=False)
     last_name = db.Column(db.String(30), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), unique=False, nullable=False)
 
-    def __init__(self, first_name, last_name, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-
 
 class Profiles(db.Model):
+    __tablename__ = "profiles"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        "user.id", ondelete="CASCADE"))
     profile_name = db.Column(db.String(50), unique=False, nullable=False)
     profile_type = db.Column(db.String(50), unique=False, nullable=False)
     user = db.relationship("User", backref="profiles")
     assigned_tasks = db.relationship("AssignedTasks", backref="profile")
 
-    def __init__(self, user_id, profile_name, profile_type):
-        self.user_id = user_id
-        self.profile_name = profile_name
-        self.profile_type = profile_type
-
 
 class Tasks(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
-    task_title = db.Column(db.String(50), unique=False, nullable=False)
+    __tablename__ = "tasks"
+    id = db.Column(db.Integer, primary_key=True)
+    task_title = db.Column(db.String(50), unique=True, nullable=False)
     task_desc = db.Column(db.String(100), unique=False, nullable=False)
     task_weight = db.Column(db.Integer, unique=False, nullable=False)
     assignments = db.relationship("AssignedTasks", backref="task")
 
-    def __init__(self, task_title, task_desc, task_weight):
-        self.task_title = task_title
-        self.task_desc = task_desc
-        self.task_weight = task_weight
-
-    def add_list_to_tasksdb():
-        db.session.query(AssignedTasks).filter(AssignedTasks.task_id >= 0).delete()
-        db.session.query(Tasks).filter(Tasks.task_id >= 0).delete()
-        db.session.execute(text("ALTER TABLE Tasks AUTO_INCREMENT = 1"))
-        db.session.commit()
-
-        tasks = [
+    def initialize_tasks():
+        default_tasks = [
             {
                 "task_title": "Dammsuga",
                 "task_desc": "Dammsug alla rum i hemmet.",
@@ -77,19 +60,26 @@ class Tasks(db.Model):
                 "task_weight": 5,
             },
             {
-                "task_title": "Putsa fönster",
-                "task_desc": "Test test test",
+                "task_title": "Tvätta kläder2",
+                "task_desc": "Fyll tvättmaskinen med nya kläder.",
                 "task_weight": 5,
             },
         ]
 
-        for task in tasks:
-            new_task = Tasks(
-                task_title=task["task_title"],
-                task_desc=task["task_desc"],
-                task_weight=task["task_weight"],
-            )
-            db.session.add(new_task)
+        tasks_dict = {task["task_title"]: task for task in default_tasks}
+
+        existing_tasks = Tasks.query.all()
+        existing_task_titles = set(task.task_title for task in existing_tasks)
+
+        for task_title, task_data in tasks_dict.items():
+            if task_title not in existing_task_titles:
+                new_task = Tasks(
+                    task_title=task_data["task_title"],
+                    task_desc=task_data["task_desc"],
+                    task_weight=task_data["task_weight"],
+                )
+                db.session.add(new_task)
+
         db.session.commit()
 
     @classmethod
@@ -100,7 +90,8 @@ class Tasks(db.Model):
                 "message": "Both title and description are required.",
             }
 
-        new_task = cls(task_title=task_title, task_desc=task_desc, task_weight=5)
+        new_task = cls(task_title=task_title,
+                       task_desc=task_desc, task_weight=5)
 
         db.session.add(new_task)
         db.session.commit()
@@ -109,12 +100,11 @@ class Tasks(db.Model):
 
 
 class AssignedTasks(db.Model):
-    task_id = db.Column(db.Integer, db.ForeignKey("tasks.task_id"), primary_key=True)
-    profile_id = db.Column(db.Integer, db.ForeignKey("profiles.id"), primary_key=True)
-
-    def __init__(self, task_id, profile_id):
-        self.task_id = task_id
-        self.profile_id = profile_id
+    __tablename__ = "assigned_tasks"
+    task_id = db.Column(db.Integer, db.ForeignKey(
+        "tasks.id", ondelete="CASCADE"), primary_key=True)
+    profile_id = db.Column(db.Integer, db.ForeignKey(
+        "profiles.id", ondelete="CASCADE"), primary_key=True)
 
     @classmethod
     def assign_task_to_profile(cls, task_id, profile_id):
